1ce7471dc422934d066c798a4d44ce9e2fb3dce9
Geme <90888249+GemeWolf@users.noreply.github.com>
2023-08-28 14:25:43 -0600

    Update 2.0

--- esx-getSharedObject-fix/server/common.lua
+++ esx-getSharedObject-fix/server/common.lua
@@ -25,72 +25,72 @@ exports('getSharedObject', function()
 end)
 
 if GetResourceState('ox_inventory') ~= 'missing' then
-  Config.OxInventory = true
-  Config.PlayerFunctionOverride = 'OxInventory'
-  SetConvarReplicated('inventory:framework', 'esx')
-  SetConvarReplicated('inventory:weight', Config.MaxWeight * 1000)
+	Config.OxInventory = true
+	Config.PlayerFunctionOverride = 'OxInventory'
+	SetConvarReplicated('inventory:framework', 'esx')
+	SetConvarReplicated('inventory:weight', Config.MaxWeight * 1000)
 end
 
 if GetResourceState('qs-inventory') ~= 'missing' then
-  Config.QSInventory = true
-  Config.PlayerFunctionOverride = 'QSInventory'
+	Config.QSInventory = true
+	Config.PlayerFunctionOverride = 'QSInventory'
 end
 
 local function StartDBSync()
-  CreateThread(function()
-    while true do
-      Wait(10 * 60 * 1000)
-      Core.SavePlayers()
-    end
-  end)
+	CreateThread(function()
+		while true do
+			Wait(10 * 60 * 1000)
+			Core.SavePlayers()
+		end
+	end)
 end
 
 MySQL.ready(function()
-  Core.DatabaseConnected = true
-  if Config.QSInventory then
-    ESX.Items = exports['qs-inventory']:GetItemList()
-  end
-  if not Config.OxInventory then
-    local items = MySQL.query.await('SELECT * FROM items')
-    for k, v in ipairs(items) do
-      ESX.Items[v.name] = {label = v.label, weight = v.weight, rare = v.rare, canRemove = v.can_remove}
-    end
-  else
-    TriggerEvent('__cfx_export_ox_inventory_Items', function(ref)
-      if ref then
-        ESX.Items = ref()
-      end
-    end)
+	Core.DatabaseConnected = true
+	if Config.QSInventory then
+		ESX.Items = exports['qs-inventory']:GetItemList()
+	end
+	if not Config.OxInventory then
+		local items = MySQL.query.await('SELECT * FROM items')
+		for _, v in ipairs(items) do
+			ESX.Items[v.name] = { label = v.label, weight = v.weight, rare = v.rare, canRemove = v.can_remove }
+		end
+	else
+		TriggerEvent('__cfx_export_ox_inventory_Items', function(ref)
+			if ref then
+				ESX.Items = ref()
+			end
+		end)
 
-    AddEventHandler('ox_inventory:itemList', function(items)
-      ESX.Items = items
-    end)
+		AddEventHandler('ox_inventory:itemList', function(items)
+			ESX.Items = items
+		end)
 
-    while not next(ESX.Items) do
-      Wait(0)
-    end
-  end
+		while not next(ESX.Items) do
+			Wait(0)
+		end
+	end
 
-  ESX.RefreshJobs()
+	ESX.RefreshJobs()
 
-  print(('[^2INFO^7] ESX ^5Legacy %s^0 initialized!'):format(GetResourceMetadata(GetCurrentResourceName(), "version", 0)))
-    
-  StartDBSync()
-  if Config.EnablePaycheck then
+	print(('[^2INFO^7] ESX ^5Legacy %s^0 initialized!'):format(GetResourceMetadata(GetCurrentResourceName(), "version", 0)))
+
+	StartDBSync()
+	if Config.EnablePaycheck then
 		StartPayCheck()
 	end
 end)
 
 RegisterServerEvent('esx:clientLog')
 AddEventHandler('esx:clientLog', function(msg)
-  if Config.EnableDebug then
-    print(('[^2TRACE^7] %s^7'):format(msg))
-  end
+	if Config.EnableDebug then
+		print(('[^2TRACE^7] %s^7'):format(msg))
+	end
 end)
 
 RegisterNetEvent("esx:ReturnVehicleType", function(Type, Request)
-  if Core.ClientCallbacks[Request] then
-    Core.ClientCallbacks[Request](Type)
-    Core.ClientCallbacks[Request] = nil
-  end
+	if Core.ClientCallbacks[Request] then
+		Core.ClientCallbacks[Request](Type)
+		Core.ClientCallbacks[Request] = nil
+	end
 end)
